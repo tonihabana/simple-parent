69bbc669b2e8eed341846c8ceb2c437dc3e98a78
toni <amdcsp@gmail.com>
2012-10-07 23:05:56 +0200

    Unbreaking everything.

--- simple-webapp/src/main/java/org/sonatype/mavenbook/web/WeatherServlet.java
+++ simple-webapp/src/main/java/org/sonatype/mavenbook/web/WeatherServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class WeatherServlet extends HttpServlet {
-    public void doGet(HttpServletRequest requestz,
+    public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
         throws ServletException, IOException {
 	String zip = request.getParameter("zip" );
